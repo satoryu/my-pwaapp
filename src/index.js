e2e624d2a19e3aa3206a06fb0b489370c5b16af9
Tatsuya Sato <satoryu.1981@gmail.com>
2019-07-30 14:58:16 +0900

    Load service worker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./sw.js");
+  })
+}
+
 import Vue from 'vue';
 import App from './App.vue';
 
