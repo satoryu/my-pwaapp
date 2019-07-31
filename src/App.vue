504570b1d0058544ee070634ba574880f3325152
Tatsuya Sato <satoryu.1981@gmail.com>
2019-07-31 11:30:23 +0900

    Preview selected file

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
     <div>
         <h1>{{ greeting }} {{ name }}!</h1>
+
+        <input type="file" @change.prevent="chooseFiles">
+
+        <div id="image-preview" v-if="file">
+            <image-preview v-bind:file="file"></image-preview>
+        </div>
     </div>
 </template>
 
 <script>
+import ImagePreview from './ImagePreview.vue'
+
 export default {
+    components: { ImagePreview },
     data: function() {
         return {
             greeting: 'Hello,',
-            name: 'Vue Component'
+            name: 'Vue Component',
+            file: null
+        }
+    },
+    methods: {
+        chooseFiles: function(event) {
+            if (event.target && event.target.files.length > 0) {
+                this.file = event.target.files[0];
+            }
         }
     }
 }
---
+++ src/ImagePreview.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="preview">
+        <img v-bind:src="dataUrl" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    props: [ 'file' ],
+    computed: {
+        dataUrl: function() {
+            return URL.createObjectURL(this.file)
+        }
+    }
+}
+</script>
+
