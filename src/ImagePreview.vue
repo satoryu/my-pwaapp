1bb44ebc68d86a6fac27b2c95890c773117b9c9a
Tatsuya Sato <satoryu.1981@gmail.com>
2019-10-23 16:54:10 +0900

    Remove capture camera option from accept attribute

--- src/ImagePreview.vue
+++ src/ImagePreview.vue
@@ -1,57 +1,56 @@
 <template>
-    <div class="preview">
-        <input type="file" accept="image/*;capture=camera" @change.prevent="chooseFiles">
+  <div class="preview">
+    <input type="file" accept="image/*" @change.prevent="chooseFiles" />
 
-        <div v-if="file">
-            <img v-bind:src="dataUrl" alt="">
+    <div v-if="file">
+      <img v-bind:src="dataUrl" alt />
 
-            <div @click.prevent="uploadPhoto">
-                <v-btn>
-                    <v-icon>mdi-upload</v-icon>
-                    Upload
-                </v-btn>
-            </div>
-        </div>
+      <div @click.prevent="uploadPhoto">
+        <v-btn>
+          <v-icon>mdi-upload</v-icon>Upload
+        </v-btn>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import { postPhoto } from '@/lib/photo-api'
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
+import { postPhoto } from "@/lib/photo-api";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 export default {
-    data: function() {
-        return {
-            file: null,
-            dataUrl: null
-        }
-    },
-    watch: {
-        file: function() {
-            this.loadFileAsDataURL()
-        }
-    },
-    methods: {
-        uploadPhoto: function() {
-            postPhoto(this.file.name, this.dataUrl)
-        },
-        loadFileAsDataURL: function() {
-            if (!this.file) {
-                return
-            }
-
-            const fileReader = new FileReader()
-            fileReader.onload = (event) => {
-                this.dataUrl = event.target.result
-            }
-            fileReader.readAsDataURL(this.file)
-        },
-        chooseFiles: function(event) {
-            if (event.target && event.target.files.length > 0) {
-                this.file = event.target.files[0];
-            }
-        }
+  data: function() {
+    return {
+      file: null,
+      dataUrl: null
+    };
+  },
+  watch: {
+    file: function() {
+      this.loadFileAsDataURL();
     }
-}
+  },
+  methods: {
+    uploadPhoto: function() {
+      postPhoto(this.file.name, this.dataUrl);
+    },
+    loadFileAsDataURL: function() {
+      if (!this.file) {
+        return;
+      }
+
+      const fileReader = new FileReader();
+      fileReader.onload = event => {
+        this.dataUrl = event.target.result;
+      };
+      fileReader.readAsDataURL(this.file);
+    },
+    chooseFiles: function(event) {
+      if (event.target && event.target.files.length > 0) {
+        this.file = event.target.files[0];
+      }
+    }
+  }
+};
 </script>
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const webpack = require('webpack');
-const path = require('path');
+require("dotenv").config();
+const webpack = require("webpack");
+const path = require("path");
 
 /*
  * SplitChunksPlugin is enabled by default and replaced
@@ -23,164 +23,164 @@ const path = require('path');
  *
  */
 
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				include: [path.resolve(__dirname, 'src')],
-				loader: 'babel-loader',
+  module: {
+    rules: [
+      {
+        include: [path.resolve(__dirname, "src")],
+        loader: "babel-loader",
 
-				options: {
-					plugins: ['syntax-dynamic-import'],
+        options: {
+          plugins: ["syntax-dynamic-import"],
 
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								modules: false
-							}
-						]
-					]
-				},
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                modules: false
+              }
+            ]
+          ]
+        },
 
-				test: /\.js$/
-			},
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader'
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.styl$/,
-				use: ['style-loader', 'css-loader', 'stylus-loader']
-			},
-			{
-				test: /\.(woff(2)?|ttf|eot|svg)/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							outputPath: 'fonts/'
-						}
-					}
-				]
-			},
-			{
-				test: /\.s(c|a)ss$/,
+        test: /\.js$/
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.styl$/,
+        use: ["style-loader", "css-loader", "stylus-loader"]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(c|a)ss$/,
 
-				use: [
-					{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader'
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							implementation: require('sass'),
-							fiber: require('fibers'),
-							indentedSyntax: true
-						}
-					}
-				]
-			}
-		]
-	},
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+              fiber: require("fibers"),
+              indentedSyntax: true
+            }
+          }
+        ]
+      }
+    ]
+  },
 
-	output: {
-		chunkFilename: '[name].[chunkhash].js',
-		filename: '[name].[chunkhash].js'
-	},
+  output: {
+    chunkFilename: "[name].[chunkhash].js",
+    filename: "[name].[chunkhash].js"
+  },
 
-	plugins: [
-		new VueLoaderPlugin(),
-		new VuetifyLoaderPlugin(),
-		new HtmlWebpackPlugin({
-			template: './src/index.html'
-		}),
-		new CleanWebpackPlugin(),
-		new webpack.DefinePlugin({
-			PHOTO_API_HOST: JSON.stringify(process.env.PHOTO_API_HOST),
-			PHOTO_API_FUNCTION_KEY: JSON.stringify(process.env.PHOTO_API_FUNCTION_KEY)
-		}),
-		new webpack.SourceMapDevToolPlugin({
-			filename: '[name].js.map'
-		}),
-		new WorkboxWebpackPlugin.GenerateSW({
-			include: [/\.(html|js|css|png|woff(2)|ttf|eot|svg)$/],
-			skipWaiting: true,
-			clientsClaim: true,
-			swDest: "./sw.js"
-		}),
-		new WebpackPwaManifest({
-			name: "My PWA Demo App",
-			short_name: "My PWA",
-			description: "This is a sandbox.",
-			display: "standalone",
-			background_color: '#FFFFFF',
-			theme_color: '#8FE3D9',
-			start_url: "/?utm_source=web_app_manifest",
-			ios: true,
-			icons: [
-				{
-					src: path.resolve('src/assets/icon.png'),
-					sizes: [96, 128, 192, 256, 384, 512]
-				},
-				{
-					src: path.resolve('src/assets/icon.png'),
-					sizes: [120, 152, 167, 180, 1024],
-					destination: path.join('icons', 'ios'),
-					inject: true,
-					ios: true
-				},
-				{
-					src: path.resolve('src/assets/icon.png'),
-					size: 1024,
-					destination: path.join('icons', 'ios'),
-					ios: 'startup'
-				}
-			]
-		})
-	],
+  plugins: [
+    new VueLoaderPlugin(),
+    new VuetifyLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      PHOTO_API_HOST: JSON.stringify(process.env.PHOTO_API_HOST),
+      PHOTO_API_FUNCTION_KEY: JSON.stringify(process.env.PHOTO_API_FUNCTION_KEY)
+    }),
+    new webpack.SourceMapDevToolPlugin({
+      filename: "[name].js.map"
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      include: [/\.(html|js|css|png|woff(2)|ttf|eot|svg)$/],
+      skipWaiting: true,
+      clientsClaim: true,
+      swDest: "./sw.js"
+    }),
+    new WebpackPwaManifest({
+      name: "My PWA Demo App",
+      short_name: "My PWA",
+      description: "This is a sandbox.",
+      display: "standalone",
+      background_color: "#FFFFFF",
+      theme_color: "#8FE3D9",
+      start_url: "/?utm_source=web_app_manifest",
+      ios: true,
+      icons: [
+        {
+          src: path.resolve("src/assets/icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512]
+        },
+        {
+          src: path.resolve("src/assets/icon.png"),
+          sizes: [120, 152, 167, 180, 1024],
+          destination: path.join("icons", "ios"),
+          inject: true,
+          ios: true
+        },
+        {
+          src: path.resolve("src/assets/icon.png"),
+          size: 1024,
+          destination: path.join("icons", "ios"),
+          ios: "startup"
+        }
+      ]
+    })
+  ],
 
-	resolve: {
-		alias: {
-			'@': path.resolve('./src'),
-			vue$: "vue/dist/vue.esm.js"
-		}
-	},
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),
+      vue$: "vue/dist/vue.esm.js"
+    }
+  },
 
-	mode: 'development',
+  mode: "development",
 
-	optimization: {
-		runtimeChunk: 'single',
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendors',
-					chunks: 'all'
-				}
-			},
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all"
+        }
+      },
 
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
-	}
+      chunks: "async",
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  }
 };
